72bb9c9f8a78e0a87014e6a2ac777ca8b7350b49
Wijkarn <mathias.wijk@hotmail.com>
2023-08-31 17:55:37 +0200

    2 commit

--- app/src/main/java/com/example/lifecycle/MainActivity.java
+++ app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -2,20 +2,79 @@ package com.example.lifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    SensorManager sm;
+    SensorEventListener listener;
+    Sensor light;
+    float lightValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView im = findViewById(R.id.imageView);
-        im.setRotation(180f);
+        //spinner();
+        orientation();
 
+        light();
+
+
+    }
+
+    public void light() {
+
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        light = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        listener = new SensorEventListener(){
+
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float newLightValue = sensorEvent.values[0];
+                if (lightValue != newLightValue){
+                    Log.d("Wijk", String.valueOf(newLightValue));
+                    lightValue = newLightValue;
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        sm.registerListener(listener, light, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void orientation() {
+        int orientation = getResources().getConfiguration().orientation;
+        Spinner spinnerPort;
+        ArrayAdapter<CharSequence> adapter;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // In landscape
+            spinnerPort = findViewById(R.id.spinner_land);
+            adapter = ArrayAdapter.createFromResource(this, R.array.dropdown_land, android.R.layout.simple_spinner_item);
+        } else {
+            // In portrait
+            spinnerPort = findViewById(R.id.spinner_port);
+            adapter = ArrayAdapter.createFromResource(this, R.array.dropdown_port, android.R.layout.simple_spinner_item);
+        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerPort.setAdapter(adapter);
+    }
+
+    public void spinner() {
+        Spinner spinnerPort = findViewById(R.id.spinner_port);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.dropdown_port, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerPort.setAdapter(adapter);
     }
 
     @Override
@@ -46,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.d("Wijk", "onPause");
+        sm.unregisterListener(listener, light);
     }
 
     @Override
